492f10a7caa53730bb794e4c7faf330c7b950f05
DickHurtz <39329057+DickHurtz@users.noreply.github.com>
2018-12-16 11:16:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ var test = document.getElementById("test");
 var btn = document.getElementById("clickme");
 
 btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'data.json');
-ourRequest.onload = function(){
+	console.log("opa!");
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'data.json');
+	ourRequest.onload = function(){
 	var ourData = JSON.parse(ourRequest.responseText);
 	console.log(ourData);
 	renderHTML(ourData);
-}
-ourRequest.send();
+	};
+	ourRequest.send();
 });
 
 function renderHTML(data){
