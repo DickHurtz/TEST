af411a850851cfb489df71247f2f038e8cb0d840
DickHurtz <39329057+DickHurtz@users.noreply.github.com>
2018-12-16 11:13:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var test = document.getElementById("test");
+var btn = document.getElementById("clickme");
+
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'data.json');
 ourRequest.onload = function(){
@@ -7,6 +10,7 @@ ourRequest.onload = function(){
 	renderHTML(ourData);
 }
 ourRequest.send();
+});
 
 function renderHTML(data){
 	var htmlString = "testtest";
